6ac9bd51632ca0af07de1117abc4db7b156c5d57
kaseyk9 <23kkazliner@athenian.org>
2021-09-03 14:34:30 -0700

    week 1 day 3

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MyMain {
     // Adds 5 to the given integer
     // This method has been written for you
@@ -8,17 +10,40 @@ public class MyMain {
     // Given three ints x, y, and z, this method
     // returns the largest of the three numbers
     public static int findBiggestNumber(int x, int y, int z) {
-        // REPLACE THIS WITH YOUR CODE
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("enter number");
+//        int x = scan.nextInt();
+//        System.out.println("enter another number");
+//        int y = scan.nextInt();
+//        System.out.println("enter another number");
+//        int z = scan.nextInt();
+
+        if (x>y && x>z) {
+        System.out.println(x + " is the biggest number");}
+        else if (y>z && y>x){
+        System.out.println(y + " is the biggest number");}
+        else if (z>x && z>y){
+        System.out.println(y + " is the biggest number");}
+
+
         return 0;
     }
 
     // This method simulates the flip of a coin, where
     // true represents "heads" and false represents "tails"
     // This method should return true half the time, and false the other half
-    public static boolean flipCoin() {
+//    public static boolean flipCoin()
         // REPLACE THIS WITH YOUR CODE
-        return false;
-    }
+//        if (Math.random() >= 0.5) {
+//            return true;
+//        }
+//        else {
+//            return false;
+
+           // return (Math.random() >= 0.5);
+
+
+
 
     // This method simulates the flip of a weighted coin,
     // that is a coin that is biased towards heads
@@ -35,21 +60,40 @@ public class MyMain {
 
     // This method returns a random number between 1 and 6, inclusive
     public static int rollDie() {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+        return (int) (Math.random() * 6) + 1;
     }
+
+//        if (r>0 && r<1){
+//        return 1;
+//        else if (r>1 && r<2){
+//        return 2;
+//        else if (r>2 && r<3){
+//        return 2;
+//        else if (r>3 && r<4){
+//        return 3;
+//        else if (r>4 && r<5){
+//        return 4;
+//                        }
+//        else if (r>5 && r<5.99999999){
+//        return 5;
+//                    }
+//                    }
+//
+//        }
+//    }
 
     // This method carries out the quadratic formula and *prints* out the answers
-    public static void quadForm(int a, int b, int c) {
-        return;
-    }
-
+//    public static void quadForm(int a, int b, int c) {
+//        return;
+//    }
+//
     public static void main(String[] args) {
         // Test the add5 method
         int x = 2;
         int y = add5(x);
         System.out.println(y);
 
-        // YOUR CODE HERE
+        System.out.println(rollDie());
     }
 }
+
